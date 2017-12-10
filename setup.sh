f54bc9861acf71609821ff81c8e6df21495b652d
Th3J0k3r <33673319+ethicalhackingplayground@users.noreply.github.com>
2017-12-10 14:10:16 +1100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo "[+] Installing mailsploit\n"
+
+pip install color_print
+pip install lazyme
+pip install ConfigParser
+pip install yagmail
+
+
+echo "\n[+] Creating some directorys."
+mkdir reports
